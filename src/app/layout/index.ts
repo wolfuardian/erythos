98efdedf3a7a607ea7afc206a59e5a9fabe8939c
Eos <80536813+wolfuardian@users.noreply.github.com>
2026-04-24 11:04:59 +0800

    [app] Wave 4-2: cornerDragStore + AreaCornerHandle pointerdown (#545)
    * chore: AT write task spec for #542
    
    * [app] Wave 4-2: cornerDragStore + AreaCornerHandle pointerdown (refs #542)

--- src/app/layout/index.ts
+++ src/app/layout/index.ts
@@ -3,3 +3,4 @@ export { AreaSplitter } from './AreaSplitter';
 export { WorkspaceTabBar } from './WorkspaceTabBar';
 export { WorkspaceTab } from './WorkspaceTab';
 export { WorkspaceContextMenu } from './WorkspaceContextMenu';
+export { AreaCornerHandle } from './AreaCornerHandle';
